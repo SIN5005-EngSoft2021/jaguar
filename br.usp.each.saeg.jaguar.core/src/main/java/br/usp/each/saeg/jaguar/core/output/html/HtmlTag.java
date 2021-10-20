01f7b511cd30f09f13dd4f9c18695bcb16024abf
Alex <alex.barboza@usp.br>
2021-10-19 21:03:49 -0300

    htmlbuilder created

---
+++ br.usp.each.saeg.jaguar.core/src/main/java/br/usp/each/saeg/jaguar/core/output/html/HtmlTag.java
@@ -0,0 +1,61 @@
+package br.usp.each.saeg.jaguar.core.output.html;
+
+public class HtmlTag {
+    private String id = "";
+    private String tag = "";
+    private String cssClass = "";
+    private String innerHtml = "";
+    private String siblingHtml = "";
+    private String inlineStyle = "";
+
+    private StringBuilder finalTag;
+
+    public HtmlTag(String id, String tag, String cssClass, String siblingHtml, String innerHtml, String inlineStyle) {
+        this.id = id;
+        this.tag = tag;
+        this.cssClass = cssClass;
+        this.innerHtml = innerHtml;
+        this.inlineStyle = inlineStyle;
+
+        this.finalTag = new StringBuilder();
+    }
+
+    public String buildTag() {
+        finalTag.append("<")
+                .append(tag);
+
+        if (!id.isEmpty()) {
+            finalTag.append(" id=\"")
+                    .append(id)
+                    .append("\"");
+        }
+
+        if (!cssClass.isEmpty()) {
+            finalTag.append(" class=\"")
+                    .append(cssClass)
+                    .append("\"");
+        }
+
+        if (!inlineStyle.isEmpty()) {
+            finalTag.append(" style=\"")
+                    .append(inlineStyle)
+                    .append("\"");
+        }
+
+        finalTag.append(">");
+
+        if (!innerHtml.isEmpty()) {
+            finalTag.append(innerHtml);
+        }
+
+        finalTag.append("</")
+                .append(tag)
+                .append(">");
+
+        if (!siblingHtml.isEmpty()) {
+            finalTag.append(siblingHtml);
+        }
+
+        return this.finalTag.toString();
+    }
+}
---
+++ br.usp.each.saeg.jaguar.core/src/main/java/br/usp/each/saeg/jaguar/core/output/html/HtmlTagBuilder.java
@@ -0,0 +1,46 @@
+package br.usp.each.saeg.jaguar.core.output.html;
+
+public class HtmlTagBuilder {
+    private String id = "";
+    private String tag = "";
+    private String cssClass = "";
+    private String innerHtml = "";
+    private String inlineStyle = "";
+    private String siblingHtml = "";
+
+    private HtmlTagBuilder() {}
+
+    public HtmlTagBuilder(String tag) {
+        this.tag = tag;
+    }
+
+    public HtmlTagBuilder setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public HtmlTagBuilder setCssClass(String cssClass) {
+        this.cssClass = cssClass;
+        return this;
+    }
+
+    public HtmlTagBuilder setInnerHtml(String innerHtml) {
+        this.innerHtml = innerHtml;
+        return this;
+    }
+
+    public HtmlTagBuilder setInlineStyle(String inlineStyle) {
+        this.inlineStyle = inlineStyle;
+        return this;
+    }
+
+    public HtmlTagBuilder setSiblingHtml(String siblingHtml) {
+        this.siblingHtml = siblingHtml;
+        return this;
+    }
+
+    public String build() {
+        HtmlTag tag = new HtmlTag(id, this.tag, cssClass, siblingHtml, innerHtml, inlineStyle);
+        return tag.buildTag();
+    }
+}
